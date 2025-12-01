e8bd3b1e53fcf98cb67c4002bd45a3b41e44a84f
lizhuang <1220484430@qq.com>
2025-12-01 15:43:19 +0800

    中国石油招标投标网--逆向练习

--- 中国石油招标投标网/decryption.js
+++ 中国石油招标投标网/decryption.js
@@ -1,25 +1,7 @@
 const { JSEncrypt } = require('./jsencrypt.js')
 
-// var data={
-//     "current": 2,
-//     "size": 10,
-//     "condition": {
-//         "columnId": "4",
-//         "title": "",
-//         "projectType": ""
-//     }
-// }
-// const datas = Buffer.from(JSON.stringify(data)).toString('base64')
-// const localStorage={
-//     "logo2": "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJj8JCLCtGj+fPBwm7m8iWQUDWrq5760edZjs00ApubrTdLQhLkHVbJv22AQ3Ntmhufwl//qvLs27cp8pCpau9fqzJc1XI5mkaYq3zg7PQ5u5p7bBchE6P5fmi+R2uMmojpR8X6zA1hJTS5ZsE0bou8Re6sWjnqV5u2hJeXPbScxAgMBAAECgYA+cONa4Ld8Byr9hCi7VY2KMHkNg5VVDBqSe50KN9Lne3EHM56IWssKiocynY9XaXB0qImRpcCkdRX3SIpE00XJh74BqPLaj07CNnq4gRnvO0v40/QvG9U1PVsmxDt47GOm+t9Kfs6EPMgMP2Wyw7sJ9sEpcN7YGSXosPXnrhKmsQJBANm9d510ifwwsBufwywwnmlid82r5dghYYJEdhW2/rx0+sRzFXrW3jBZ2Pu5+12/H7wDqkL9eUM7CY3wKbOj1A0CQQCz3dAGa8TPDBrFAD6dO4pH2NmPmGN0HgObiMWsi5YrBoDngFTGqJOZkKlt+8+qaATfeTUNVLigOILXKzOACKK1AkEAm5TSH2PiJJz1eQeTAcRLrKl7SS6GsQRJFDeu2J1FL4u2kyBYPMnDQXExpcyiW73xAvrrcaqENxG8JftxELcDEQJAfQD8cWn2ltrXw/A+k/HsUbCQy25iODIf2bl9gERmsjJL5hhXUtWX9xMF3Y0zL0ApmtJXH5Ow2JFNxZKdsS7PPQJBANWuhzmTm9NkeCqRNBQ/PMeAaPZuhs/+NB4j4bmVm6TY+iRowecZ+bmbD+vWr0JcBo3Cllhbloz39qz7TfORhok= ",
-//     "logo1": "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCI7UP802rorCeicqPg+F81psNnQ8Gslx4cIAiRWHlaK5w3iraC2UURhgyFwEXO+/Q443SxgLmWY1NrLej7JmiOnILgO+nxOI/qhPMbTDSJSeyRSqt55dzJItKhymTYhCkdKxxmjBE/LKlxS7e57wSIjA1uy4WUDb4VFq2SXfTrrwIDAQAB ",
-//     "time": "1764397446805"
-// }
-
-
 function decrypt_params(data,localStorage){
     const datas = Buffer.from(JSON.stringify(data)).toString('base64')
-
     var encryptor = new JSEncrypt()
     encryptor.setPublicKey(localStorage.logo1)
     data = encryptor.encryptLong(JSON.stringify(datas))
@@ -31,6 +13,5 @@ function decrypt_result(res,localStorage){
     encryptorJm.setPrivateKey(localStorage.logo2)
     var result = encryptorJm.decryptLong(res)
     const ResultData = Buffer.from(result, 'base64').toString('utf8')
-    const ele = JSON.parse(ResultData)
-    return ele
+    return JSON.parse(ResultData)
 }
--- 中国石油招标投标网/spiders.py
+++ 中国石油招标投标网/spiders.py
@@ -3,111 +3,144 @@ import requests
 import re
 import time
 from fake_useragent import UserAgent
-
-# 初始化 UserAgent 对象
-ua = UserAgent()
-print(ua.random)
-def get_random_headers():
-    """生成带有随机 User-Agent 的请求头"""
-    return {
-        "Content-Type": "application/json;charset=UTF-8",
-        "MACHINE_CODE": "null",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
-
-    }
-
-time_stamp = str(int(time.time() * 1000))
-headers = get_random_headers()
-
-def get_local_storage(headers=None):
-    """获取 localStorage，使用传入的 headers 或生成新的随机 headers"""
-    if headers is None:
-        headers = get_random_headers()
-    url = "https://www.cnpcbidding.com/cms/css/bj.css"
-    response = requests.get(url, headers=headers)
-
-    result_ls = response.text.split('.')
-
-    css1 = result_ls[1] if len(result_ls) > 1 else ''
-    css2 = result_ls[2] if len(result_ls) > 2 else ''
-
-    regex = r'base64,([^)]+)\)'
-
-    c1 = re.search(regex, css1)
-    c2 = re.search(regex, css2)
-
-    localStorage={
-        "logo2": c2.group(1),
-        "logo1": c1.group(1),
-        "time": time_stamp
-    }
-    return localStorage
-
-localStorage = get_local_storage()
-
-# 1. 编写或读取 JS 代码字符串
-with open('decryption.js', 'r') as f:
-    js_code = f.read()
-
-# 2. 编译 JS
-ctx = execjs.compile(js_code)
-
-for i in range(1, 155):
-
-    # 每次请求都生成新的随机 User-Agent
-    headers = get_random_headers()
-    time_stamp = str(int(time.time() * 1000))
-    headers["MACHINE_CODE"] = time_stamp
-
-    data={
-        "current": i,
-        "size": 10,
-        "condition": {
-            "columnId": "4",
-            "title": "",
-            "projectType": ""
+from pprint import pp, pprint
+import ddddocr
+import re
+class Spider(object):
+    def __init__(self):
+        self.ctx = None
+        self.project_type_map= {
+            "全部": "",
+            "物资": "0001",
+            "工程": "0002",
+            "服务": "0001",
         }
-    }
+        self.ocr = ddddocr.DdddOcr(show_ad=False)
+    def get_random_headers(self):
+        """生成带有随机 User-Agent 的请求头"""
+        return {
+            "Content-Type": "application/json;charset=UTF-8",
+            "MACHINE_CODE": str(int(time.time() * 1000)),
+            "User-Agent": UserAgent().random,
+
+        }
+
+    def get_local_storage(self,headers=None):
+        url = "https://www.cnpcbidding.com/cms/css/bj.css"
+        response = requests.get(url, headers=headers)
+        result_ls = response.text.split('.')
+        regex = r'base64,([^)]+)\)'
+        c1 = re.search(regex, result_ls[1])
+        c2 = re.search(regex, result_ls[2])
+
+        localStorage={
+            "logo2": c2.group(1),
+            "logo1": c1.group(1),
+            "time": headers['MACHINE_CODE']
+        }
+        return localStorage
+    
+    def read_js_code(self,file_path='decryption.js'):
+        # 1. 编写或读取 JS 代码字符串
+        with open(file_path, 'r') as f:
+            js_code = f.read()
+        return execjs.compile(js_code)
 
 
-    # 使用当前的 headers 获取 localStorage（保持 User-Agent 一致）
-    localStorage = get_local_storage(headers)
-    # 3. 调用 JS 函数
-    decrypt_params = ctx.call("decrypt_params", data,localStorage)
+    def get_encrypt_data(self,request_params:dict=None,headers=None,localStorage=None):
+        data={
+            "current": request_params['page'],
+            "size": 10,
+            "condition": {
+                "columnId": "4",# 公开招标中标候选人公示
+                "title": request_params['title'],# 搜索关键字
+                "projectType": request_params['project_type'],#项目类型
+            }
+        }
+        decrypt_params = self.ctx.call("decrypt_params", data,localStorage)
 
-    url = "https://www.cnpcbidding.com/cms/article/page"
+        request_url= "https://www.cnpcbidding.com/cms/article/page"
+        response = requests.post(request_url, headers=headers, data=decrypt_params)
+        return response
 
-    response = requests.post(url, headers=headers, data=decrypt_params)
-    # print(response.text)
-    if '"code":"511"' in response.text:
+    def get_captcha(self,headers=None):
         qr_img_url = "https://www.cnpcbidding.com/cms/validateCode/undefined"
         response = requests.get(qr_img_url, headers=headers)
         img_data=response.json()['data']
-
-        qr_url='http://api.jfbym.com/api/YmServer/customApi'
-        qr_data={
-        "token": "b-SkqpN3hVoR2syRpa2T_qlcMaFx559kIt3Axyxj158",
-        "type": "50100",
-        "image": img_data,
-    }
-        response = requests.post(qr_url, headers=headers, json=qr_data)
-        resutl_jisuan=response.json()['data']['data']
-
-        yz_url=f'https://www.cnpcbidding.com/cms/validateCode/{resutl_jisuan}'
-        response = requests.get(yz_url, headers=headers)
-        js_code = response.json()
-        print(js_code)
+        return img_data
     
-        response = requests.post(url, headers=headers, data=decrypt_params)
-        try:
-            decrypt_result = ctx.call("decrypt_result", response.text,localStorage)
+    def get_captcha_result(self,ocr_qr_result):
+        # 先去除空白字符和等号后的内容
+        expr = ocr_qr_result.replace(' ', '').replace('＝', '=')
+        expr = re.sub(r'=.*', '', expr)  # 去除等号及后面内容
+
+        # 统一常见的算术符号
+        expr = expr.replace('＋', '+').replace('t', '+').replace('T', '+')
+        expr = expr.replace('x', '*').replace('X', '*').replace('×', '*')
+        expr = expr.replace('÷', '/').replace('·', '*')
+
+        # 移除除数字和运算符外的其他字符
+        expr = re.sub(r'[^0-9\+\-\*/]', '', expr)
+        return eval(expr)
+
+    def verify_captcha(self,captcha_result,headers=None):
+        yz_url=f'https://www.cnpcbidding.com/cms/validateCode/{captcha_result}'
+        response = requests.get(yz_url, headers=headers)
+        qr_response_data = response.json()
+        if qr_response_data['message'] == "请求成功":
+            return qr_response_data
+
+    def loop_handle_captcha(self,headers=None):
+        loop_num = 10
+        while True:
+            # 获取验证码base64信息
+            img_data = self.get_captcha(headers)
+            # 调用ddddocr识别验证码
+            ocr_qr_result = self.ocr.classification(img_data)
+            # 去除验证码不干净数据，计算验证码结果
+            captcha_result = self.get_captcha_result(ocr_qr_result)
+            # 发起请求，验证验证码是否正确
+            verify_flag = self.verify_captcha(captcha_result,headers)
+            if verify_flag:
+                return verify_flag
+            else:
+                loop_num = loop_num - 1
+
+        
+
+    def run(self):
+        headers = self.get_random_headers()
+        localStorage = self.get_local_storage(headers)
+        self.ctx = self.read_js_code()
+        request_params={
+            'page': 10,
+            'title': "",
+            'project_type': ""
+        }
+        encrypt_data_response = self.get_encrypt_data(request_params,headers,localStorage)
+        if '"code":"511"' in encrypt_data_response.text:
+            verify_flag = self.loop_handle_captcha(headers)
+            if verify_flag:
+                encrypt_data_response = self.get_encrypt_data(request_params,headers,localStorage)
+                decrypt_result = self.ctx.call("decrypt_result", encrypt_data_response.text,localStorage)
+                print(decrypt_result['data']['current'])
+        else:
+            decrypt_result = self.ctx.call("decrypt_result", encrypt_data_response.text,localStorage)
             print(decrypt_result['data']['current'])
-        except Exception as e:
-            print('解析失败','第',i,'页')
-        continue
-    response = requests.post(url, headers=headers, data=decrypt_params)
-    decrypt_result = ctx.call("decrypt_result", response.text,localStorage)
-    print(decrypt_result['data']['current'],decrypt_result)
 
 
 
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    spider = Spider()
+    page_num = 0
+    for i in range(1, 100):
+        spider.run()
+        page_num = page_num + 1
+    print(page_num)
